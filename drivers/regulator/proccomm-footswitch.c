a917a1bbeb1e1b0f23683a2ed7b332361187c13d
Rudolf Tammekivi <rtammekivi@gmail.com>
2016-07-24 18:20:46 +0300

    regulator: Add ProcComm voltage regulator/footswitch driver
    This is initially imported from the LA.AF.1.1_rb1.17, however it is a complete
    makeover to support device tree. This change also adds footswitches, also
    imported from LA.AF.1.1_rb1.17, but greatly modified to support device tree.
    
    Change-Id: I59cc4640f6232b7d9e7984ae2884d3d24ceaf2e6

---
+++ drivers/regulator/proccomm-footswitch.c
@@ -0,0 +1,250 @@
+/*
+ * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2017, Rudolf Tammekivi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/clk.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/regulator/driver.h>
+#include <linux/regulator/machine.h>
+#include <linux/regulator/of_regulator.h>
+#include <soc/qcom/proc_comm.h>
+
+#define PCOM_RAIL_MODE_AUTO	0
+#define PCOM_RAIL_MODE_MANUAL	1
+
+/*
+ * Wrappers for the msm_proc_comm() calls.
+ */
+static inline int set_rail_mode(int pcom_id, int mode)
+{
+	int rc;
+
+	rc = msm_proc_comm(PCOM_CLKCTL_RPC_RAIL_CONTROL, &pcom_id, &mode);
+	if (!rc && pcom_id)
+		rc = -EINVAL;
+
+	return rc;
+}
+
+static int set_rail_state(int pcom_id, int state)
+{
+	int rc;
+
+	rc = msm_proc_comm(state, &pcom_id, NULL);
+	if (!rc && pcom_id)
+		rc = -EINVAL;
+
+	return rc;
+}
+
+struct proccomm_footswitch_data {
+	struct regulator_desc	rdesc;
+	unsigned		id;
+	struct clk		*core_clk;
+	int			core_clk_init_rate;
+	struct clk		*iface_clk;
+
+	bool			enabled;
+};
+
+static int proccomm_fs_set_clocks(struct proccomm_footswitch_data *data,
+				  bool enable)
+{
+	if (enable) {
+		bool rate_set = !!clk_get_rate(data->core_clk);
+		if (!rate_set)
+			clk_set_rate(data->core_clk, data->core_clk_init_rate);
+		clk_prepare_enable(data->core_clk);
+		clk_prepare_enable(data->iface_clk);
+	} else {
+		clk_disable_unprepare(data->iface_clk);
+		clk_disable_unprepare(data->core_clk);
+	}
+
+	return 0;
+}
+
+static int proccomm_fs_enable(struct regulator_dev *rdev)
+{
+	int ret;
+	struct proccomm_footswitch_data *data = rdev_get_drvdata(rdev);
+
+	proccomm_fs_set_clocks(data, true);
+
+	ret = set_rail_state(data->id, PCOM_CLKCTL_RPC_RAIL_ENABLE);
+	if (ret) {
+		dev_err(rdev_get_dev(rdev),
+			"failed to enable regulator %d (%s): %d\n",
+			data->id, data->rdesc.name, ret);
+	} else {
+		dev_dbg(rdev_get_dev(rdev), "enabled regulator %d (%s)\n",
+			data->id, data->rdesc.name);
+		data->enabled = true;
+	}
+
+	proccomm_fs_set_clocks(data, false);
+
+	return ret;
+}
+
+static int proccomm_fs_disable(struct regulator_dev *rdev)
+{
+	int ret;
+	struct proccomm_footswitch_data *data = rdev_get_drvdata(rdev);
+
+	proccomm_fs_set_clocks(data, true);
+
+	ret = set_rail_state(data->id, PCOM_CLKCTL_RPC_RAIL_DISABLE);
+	if (ret) {
+		dev_err(rdev_get_dev(rdev),
+			"failed to disable regulator %d (%s): %d\n",
+			data->id, data->rdesc.name, ret);
+	} else {
+		dev_dbg(rdev_get_dev(rdev), "disabled regulator %d (%s)\n",
+			data->id, data->rdesc.name);
+		data->enabled = 0;
+	}
+
+	proccomm_fs_set_clocks(data, false);
+
+	return ret;
+}
+
+static int proccomm_fs_is_enabled(struct regulator_dev *rdev)
+{
+	struct proccomm_footswitch_data *data = rdev_get_drvdata(rdev);
+	return data->enabled;
+}
+
+static struct regulator_ops proccomm_fs_ops = {
+	.enable		= proccomm_fs_enable,
+	.disable	= proccomm_fs_disable,
+	.is_enabled	= proccomm_fs_is_enabled,
+};
+
+static int proccomm_fs_probe(struct platform_device *pdev)
+{
+	int ret;
+	struct device *dev = &pdev->dev;
+	struct device_node *node = dev->of_node;
+	struct proccomm_footswitch_data *data;
+
+	struct regulator_init_data *initdata;
+	struct regulator_desc *rdesc;
+	struct regulator_dev *rdev;
+	struct regulator_config config = { };
+
+	if (!node) {
+		dev_err(dev, "no device tree data supplied\n");
+		return -EINVAL;
+	}
+
+	data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
+	if (!data) {
+		dev_err(dev, "failed to allocate memory\n");
+		return -ENOMEM;
+	}
+
+	rdesc = &data->rdesc;
+
+	ret = of_property_read_u32(node, "id", &data->id);
+	if (ret) {
+		dev_err(dev, "failed to read 'id'\n");
+		return ret;
+	}
+
+	/*
+	 * Enable footswitch in manual mode (ie. not controlled along
+	 * with pcom clocks).
+	 */
+	ret = set_rail_state(data->id, PCOM_CLKCTL_RPC_RAIL_ENABLE);
+	if (ret) {
+		dev_err(dev, "failed to set rail state for %d\n", data->id);
+		return ret;
+	}
+
+	ret = set_rail_mode(data->id, PCOM_RAIL_MODE_MANUAL);
+	if (ret) {
+		dev_err(dev, "failed to set rail mode for %d\n", data->id);
+		return ret;
+	}
+
+	data->core_clk = devm_clk_get(dev, "core");
+	if (IS_ERR(data->core_clk))
+		data->core_clk = NULL;
+
+	of_property_read_u32(node, "core-clk-rate", &data->core_clk_init_rate);
+
+	data->iface_clk = devm_clk_get(dev, "iface");
+	if (IS_ERR(data->iface_clk))
+		data->iface_clk = NULL;
+
+	initdata = of_get_regulator_init_data(dev, node, rdesc);
+	if (!initdata) {
+		dev_err(dev, "failed to get regulator init data\n");
+		return -ENOMEM;
+	}
+
+	rdesc->name = initdata->constraints.name;
+	rdesc->ops = &proccomm_fs_ops;
+	rdesc->type = REGULATOR_VOLTAGE;
+	rdesc->owner = THIS_MODULE;
+
+	config.dev = dev;
+	config.init_data = initdata;
+	config.driver_data = data;
+	config.of_node = node;
+
+	rdev = devm_regulator_register(dev, rdesc, &config);
+	if (IS_ERR(rdev)) {
+		dev_err(dev, "failed to register regulator\n");
+		return PTR_ERR(rdev);;
+	}
+
+	platform_set_drvdata(pdev, rdev);
+
+	return 0;
+}
+
+static struct of_device_id proccomm_fs_of_match_table[] = {
+	{ .compatible = "qcom,proccomm-footswitch", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, proccomm_fs_of_match_table);
+
+static struct platform_driver proccomm_fs_driver = {
+	.probe	= proccomm_fs_probe,
+	.driver	= {
+		.name		= "proccomm-footswitch",
+		.owner		= THIS_MODULE,
+		.of_match_table	= proccomm_fs_of_match_table,
+	},
+};
+
+static int __init proccomm_fs_init(void)
+{
+	return platform_driver_register(&proccomm_fs_driver);
+}
+subsys_initcall(proccomm_fs_init);
+
+static void __exit proccomm_fs_exit(void)
+{
+	platform_driver_unregister(&proccomm_fs_driver);
+}
+module_exit(proccomm_fs_exit);
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("ProcComm footswitch driver");
+MODULE_VERSION("1.0");
---
+++ drivers/regulator/proccomm-regulator.c
@@ -0,0 +1,269 @@
+/*
+ * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2016-2017, Rudolf Tammekivi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/regulator/driver.h>
+#include <linux/regulator/machine.h>
+#include <linux/regulator/of_regulator.h>
+#include <linux/of.h>
+#include <soc/qcom/proc_comm.h>
+
+#define MV_TO_UV(mv)		((mv)*1000)
+#define UV_TO_MV(uv)		((uv)/1000)
+
+/*
+ * Wrappers for the msm_proc_comm() calls.
+ * Does basic impedance matching between what the proccomm interface
+ * expects and how the driver sees the world.
+ */
+
+/* Converts a proccomm error to an errno value. */
+static int _pcom_err_to_linux_errno(unsigned error)
+{
+	if (!error)		/* 0 == no error */
+		return 0;
+	else if (error & 0x1F)	/* bits 0..4 => parameter 1..5 out of range */
+		return -EDOM;
+	else if (error & 0x100)	/* bit 8 => feature not supported */
+		return -ENOSYS;
+	else			/* anything else non-zero: unknown error */
+		return -EINVAL;
+}
+
+/* vreg_switch: (vreg ID, on/off) => (return code, <null>) */
+static int _vreg_switch(unsigned vreg_id, bool enable)
+{
+	unsigned _enable = !!enable;
+
+	return msm_proc_comm(PCOM_VREG_SWITCH, &vreg_id, &_enable);
+}
+
+/* vreg_set_level: (vreg ID, mV) => (return code, <null>) */
+static int _vreg_set_level(unsigned vreg_id, int level_mV)
+{
+	int ret;
+	unsigned _level = (unsigned)level_mV;
+
+	ret = msm_proc_comm(PCOM_VREG_SET_LEVEL, &vreg_id, &_level);
+	if (ret)
+		return ret;
+
+	return _pcom_err_to_linux_errno(vreg_id);
+}
+
+/* vreg_pull_down: (pull down, vreg ID) => (<null>, <null>) */
+/* Returns error code from msm_proc_comm. */
+static int _vreg_pull_down(unsigned vreg_id, bool pull_down)
+{
+	unsigned _pull_down = !!pull_down;
+
+	return msm_proc_comm(PCOM_VREG_PULLDOWN, &_pull_down, &vreg_id);
+}
+
+struct proccomm_regulator_data {
+	struct regulator_desc	rdesc;
+	unsigned		id;
+	int			rise_time;
+	bool			pulldown;
+	bool			negative;
+
+	int			last_voltage;
+	bool			enabled;
+};
+
+static int proccomm_vreg_enable(struct regulator_dev *rdev)
+{
+	int ret;
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
+
+	ret = _vreg_switch(data->id, true);
+	if (ret) {
+		dev_err(rdev_get_dev(rdev),
+			"failed to enable regulator %d (%s): %d\n",
+			data->id, data->rdesc.name, ret);
+	} else {
+		dev_dbg(rdev_get_dev(rdev), "enabled regulator %d (%s)\n",
+			data->id, data->rdesc.name);
+		data->enabled = 1;
+	}
+
+	return ret;
+}
+
+static int proccomm_vreg_disable(struct regulator_dev *rdev)
+{
+	int ret;
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
+
+	ret = _vreg_switch(data->id, false);
+	if (ret) {
+		dev_err(rdev_get_dev(rdev),
+			"failed to disable regulator %d (%s): %d\n",
+			data->id, data->rdesc.name, ret);
+	} else {
+		dev_dbg(rdev_get_dev(rdev), "disabled regulator %d (%s)\n",
+			data->id, data->rdesc.name);
+		data->enabled = 0;
+	}
+
+	return ret;
+}
+
+static int proccomm_vreg_is_enabled(struct regulator_dev *rdev)
+{
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
+	return data->enabled;
+}
+
+static int proccomm_vreg_get_voltage(struct regulator_dev *rdev)
+{
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
+	return MV_TO_UV(data->last_voltage);
+}
+
+static int proccomm_vreg_set_voltage(struct regulator_dev *rdev,
+				     int min_uV, int max_uV, unsigned *sel)
+{
+	int ret;
+	struct proccomm_regulator_data *data = rdev_get_drvdata(rdev);
+	int level_mV = UV_TO_MV(min_uV);
+
+	ret = _vreg_set_level(data->id,
+			      data->negative ? -level_mV : level_mV);
+	if (ret) {
+		dev_err(rdev_get_dev(rdev),
+			"failed to set voltage for regulator %d (%s) "
+			"to %d mV: %d\n",
+			data->id, data->rdesc.name, level_mV, ret);
+	} else {
+		dev_dbg(rdev_get_dev(rdev),
+			"voltage for regulator %d (%s) set to %d mV\n",
+			data->id, data->rdesc.name, level_mV);
+		data->last_voltage = level_mV;
+	}
+
+	return ret;
+}
+
+static struct regulator_ops proccomm_vreg_ops = {
+	.enable		= proccomm_vreg_enable,
+	.disable	= proccomm_vreg_disable,
+	.is_enabled	= proccomm_vreg_is_enabled,
+	.get_voltage	= proccomm_vreg_get_voltage,
+	.set_voltage	= proccomm_vreg_set_voltage,
+};
+
+static int proccomm_vreg_probe(struct platform_device *pdev)
+{
+	int ret;
+	struct device *dev = &pdev->dev;
+	struct device_node *node = dev->of_node;
+	struct proccomm_regulator_data *data;
+
+	struct regulator_init_data *initdata;
+	struct regulator_desc *rdesc;
+	struct regulator_dev *rdev;
+	struct regulator_config config = { };
+
+	if (!node) {
+		dev_err(dev, "no device tree data supplied\n");
+		return -EINVAL;
+	}
+
+	data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
+	if (!data) {
+		dev_err(dev, "failed to allocate memory\n");
+		return -ENOMEM;
+	}
+
+	rdesc = &data->rdesc;
+
+	ret = of_property_read_u32(node, "id", &data->id);
+	if (ret) {
+		dev_err(dev, "failed to read 'id' ret=%d\n", ret);
+		return ret;
+	}
+
+	of_property_read_u32(node, "rise-time", &rdesc->enable_time);
+	data->pulldown = of_property_read_bool(node, "pulldown");
+	data->negative = of_property_read_bool(node, "negative");
+
+	if (data->pulldown) {
+		ret = _vreg_pull_down(data->id, data->pulldown);
+		if (ret) {
+			dev_err(dev, "failed to pull down regulator ret=%d\n",
+				ret);
+			return ret;
+		}
+	}
+
+	initdata = of_get_regulator_init_data(dev, node, rdesc);
+	if (!initdata) {
+		dev_err(dev, "failed to get regulator init data\n");
+		return -ENOMEM;
+	}
+
+	rdesc->name = initdata->constraints.name;
+	rdesc->ops = &proccomm_vreg_ops;
+	rdesc->type = REGULATOR_VOLTAGE;
+	rdesc->owner = THIS_MODULE;
+
+	config.dev = dev;
+	config.init_data = initdata;
+	config.driver_data = data;
+	config.of_node = node;
+
+	rdev = devm_regulator_register(dev, rdesc, &config);
+	if (IS_ERR(rdev)) {
+		ret = PTR_ERR(rdev);
+		dev_err(dev, "failed to register regulator ret=%d\n", ret);
+		return ret;
+	}
+
+	platform_set_drvdata(pdev, rdev);
+
+	return 0;
+}
+
+static struct of_device_id proccomm_vreg_of_match_table[] = {
+	{ .compatible = "qcom,proccomm-regulator", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, proccomm_vreg_of_match_table);
+
+static struct platform_driver proccomm_vreg_driver = {
+	.probe	= proccomm_vreg_probe,
+	.driver	= {
+		.name		= "proccomm-regulator",
+		.owner		= THIS_MODULE,
+		.of_match_table	= proccomm_vreg_of_match_table,
+	},
+};
+
+static int __init proccomm_vreg_init(void)
+{
+	return platform_driver_register(&proccomm_vreg_driver);
+}
+subsys_initcall(proccomm_vreg_init);
+
+static void __exit proccomm_vreg_exit(void)
+{
+	platform_driver_unregister(&proccomm_vreg_driver);
+}
+module_exit(proccomm_vreg_exit);
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("ProcComm regulator driver");
+MODULE_VERSION("1.0");
